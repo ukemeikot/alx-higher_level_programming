b4fba455b31453fe52759d333c3bb210da99f031
ukemeikot <ukemeetim2222@gmail.com>
2023-11-28 14:38:36 +0100

    alphabets

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for a in range(ord('z'), ord('a') - 1, -1):
+    if a % 2 != 0:
+        uppercase = ord('A') + (a -ord('a'))
+        print("{:c}".format(uppercase), end="")
+    else:
+        print("{}".format(chr(a)), end="")
