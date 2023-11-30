2daeff80999435cdc0848dc777708f5a0ae235b5
ukemeikot <ukemeetim2222@gmail.com>
2023-11-30 16:09:43 +0100

    change

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from sys import argv
+from sys import argv, exit
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
@@ -14,13 +14,12 @@ if __name__ == "__main__":
     a = int(argv[1])
     b = int(argv[3])
     operator = argv[2]
-    match operator:
-        case '+':
-            result = add(a, b)
-        case '-':
-            result = sub(a, b)
-        case '*':
-            result = mul(a, b)
-        case '/':
-            result = div(a, b)
+    if operator == '+':
+        result = add(a, b)
+    if operator == '-':
+        result = sub(a, b)
+    if operator == '*':
+        result = mul(a, b)
+    else:
+        result = div(a, b)
     print("{} {} {} = {}".format(a, operator, b, result))
