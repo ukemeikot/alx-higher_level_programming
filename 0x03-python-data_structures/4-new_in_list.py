5c718e57f32353050236840016bbd8600ba9db99
ukemeikot <ukemeetim2222@gmail.com>
2023-12-04 11:00:39 +0100

    item replacement in a list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    n_list = [x for x in my_list]
+    if idx < 0  or idx >= len(n_list):
+        return n_list
+    n_list[idx] = element
+    return n_list
