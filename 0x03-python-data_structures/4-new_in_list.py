592c9a04214f1a54d873494d880d12d0df2523bc
ukemeikot <ukemeetim2222@gmail.com>
2023-12-04 11:04:19 +0100

    changes

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 
 def new_in_list(my_list, idx, element):
     n_list = [x for x in my_list]
-    if idx < 0  or idx >= len(n_list):
+    if idx < 0 or idx >= len(n_list):
         return n_list
     n_list[idx] = element
     return n_list
