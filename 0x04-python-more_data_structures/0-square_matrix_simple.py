c70f37b0afcc6147a6e434932486ab5ff33a5090
ukemeikot <ukemeetim2222@gmail.com>
2023-12-08 09:58:48 +0100

    matrix multiplication

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    if matrix == []:
+        return []
+    new_matrix = []
+    for row in matrix:
+        new_matrix.append([item**2 for item in row])
+    return new
