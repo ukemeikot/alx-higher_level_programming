70c5e6b22e81fdda54f897a54f11162090b6de0d
ukemeikot <ukemeetim2222@gmail.com>
2023-12-08 12:35:46 +0100

    search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    if my_list == [] or my_list is None:
+        return []
+    new_list = [replace if item is search else item for item in my_list]
+    return new_list
+    
