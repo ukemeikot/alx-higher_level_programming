cbf80aafc5909cce71c8a6fa02d65429c6e96877
ukemeikot <ukemeetim2222@gmail.com>
2023-12-20 19:00:05 +0100

    printing of the square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+
+"""Square module: this class <sqaure> models a square object"""
+
+
+class Square:
+    """Square class
+     this class implements a square class with setters and getters
+
+
+
+    """
+    def __init__(self, size=0):
+        """ __init__ method
+
+
+        this method is a special method that acts as an instance
+        constructor after initialization.
+
+
+        Args:
+            size (int): <size> is a private attribute of an integer.
+
+
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Area method
+
+
+        this method returns the area of the square
+
+        Args:
+            No Args
+
+
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Getter method"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        self.__size = value
+
+    def my_print(self):
+        """Self printing method
+
+        prints out the square using # symbol
+
+
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for a in range(self.__size):
+                for b in range(self.__size):
+                    print("#", end="")
+                print()
