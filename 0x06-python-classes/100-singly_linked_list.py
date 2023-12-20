bb179aa67acdd52433971fdd3b85fa9c4c275391
ukemeikot <ukemeetim2222@gmail.com>
2023-12-20 20:10:25 +0100

    chnges2

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -36,6 +36,7 @@ class Node:
         else:
             raise TypeError("next_node must be a Node object")
 
+
 class SinglyLinkedList:
     """ a class model for a singly linked list """
     def __init__(self):
