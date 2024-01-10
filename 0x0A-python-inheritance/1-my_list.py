6b449f235ea2764f34ad254ba9fa8178205455f0
ukemeikot <ukemeetim2222@gmail.com>
2024-01-10 23:35:02 +0100

    changes

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
-
 """
-class the inherits from the list class
+contains the MyList class
 """
 
 
 class MyList(list):
-    """
-        a class that extends the functionality of the lst class
-    """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    """Implements sorted printing for the built-in list class."""
 
     def print_sorted(self):
-        """
-            print a list in a soretd order
-        """
-        cpy = self.copy()
-        cpy.sort()
-        print(cpy)
+        """Print a list in sorted ascending order."""
+        print(sorted(self))
