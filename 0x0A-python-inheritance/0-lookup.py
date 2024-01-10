034756fe9b7fcc0198244ec9520593f8cb2b22d3
ukemeikot <ukemeetim2222@gmail.com>
2024-01-10 23:15:48 +0100

    printing list of directories

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+                module documentation
+    funtion that returns the list of available attributes and methods
+    of an object
+"""
+
+
+def lookup(obj):
+    """ returns list of dir(obj) """
+    return dir(obj)
