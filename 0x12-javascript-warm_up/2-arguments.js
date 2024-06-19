375836cacd803fea562a01fe491e1535905b5abc
ukemeikot <ukemeetim2222@gmail.com>
2024-06-19 21:35:37 +0100

    correction

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 
 const args = process.argv;
 if (args.length === 2) {
-  console.log('No arguments');
+  console.log('No argument');
 } else if (args.length === 3) {
   console.log('Argument found');
 } else {
