b15154ea2a54c8ae2dd2a65cd3431798ca8dfbf9
ukemeikot <ukemeetim2222@gmail.com>
2024-06-19 22:44:52 +0100

    changes

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+let x = process.argv[2];
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  x = Math.floor(Number(x));
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
